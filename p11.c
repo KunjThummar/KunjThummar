e00a4f6848f4af792cc0f5446fbb31ec627c8b1f
KunjThummar <kunjthummar.ce@gmail.com>
2024-08-02 15:49:03 +0530

    Add files via upload

---
+++ p11.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+   int X=21,U,C,a;
+   printf("you can pick up maximum 4 matchstick.\n");
+
+   for(a=0;a<=21;a=a+U+C)
+   {
+  A: printf("enter the number of matchstick:");
+   scanf("%d",&U);
+   if(U>4)
+       {
+       printf("invalid input.\n");
+       goto A;
+       }
+   else
+    {
+   printf("\nyou have pick up %d matchstick.\n",U);
+   C=5-U;
+   printf("computer pick up %d matchstick.\n",C);
+   printf("you pick up last matchstick.\n");
+   printf("you lost the game.");}
+   }
+}
+
