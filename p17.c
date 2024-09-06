58e0461203c8c02bb29c45df05cf80668511bee9
KunjThummar <kunjthummar.ce@gmail.com>
2024-09-06 15:28:20 +0530

    Update p17.c

--- p17.c
+++ p17.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main()
 {
-    int a[5][10];
-    int n,i,r,c,x;
+    int a[5][10],Z;
+    int n,i,r,c,x,Y;
     for(r=0;r<5;r++)
     {
         for(c=0;c<10;c++)
@@ -10,7 +10,7 @@ void main()
             a[r][c] = 79;
         }
     }
-    printf("enter the number of seat:");
+    Y:printf("enter the number of seat:");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
@@ -18,10 +18,16 @@ void main()
         scanf("%d",&r);
         scanf("%d",&c);
         printf("\n");
-        a[r-1][c-1] = 88;
+        if(a[r-1][c-1]==88)
+        {
+            printf("the seat is book.");
+            goto Z;
+        }
+        else
+        {
+            a[r-1][c-1] = 88;
+        }
     }
-
-
     printf("seating chart:\n");
     for(r=0;r<5;r++)
     {
@@ -32,8 +38,8 @@ void main()
 
         }
         printf("\n");
-    }
-}
-//programmer=Kunj Thummar
+    }
+    Z:goto Y;
 
+}
 
