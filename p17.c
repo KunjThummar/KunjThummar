0032ca6858a54ce5e514beaa27610aa9f5434746
KunjThummar <kunjthummar.ce@gmail.com>
2024-09-06 15:13:39 +0530

    Add files via upload

---
+++ p17.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+    int a[5][10];
+    int n,i,r,c,x;
+    for(r=0;r<5;r++)
+    {
+        for(c=0;c<10;c++)
+        {
+            a[r][c] = 79;
+        }
+    }
+    printf("enter the number of seat:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("enter the number of seat for reserved seat %d :",i);
+        scanf("%d",&r);
+        scanf("%d",&c);
+        printf("\n");
+        a[r-1][c-1] = 88;
+    }
+
+
+    printf("seating chart:\n");
+    for(r=0;r<5;r++)
+    {
+         printf("row %d",r+1);
+        for(c=0;c<10;c++)
+        {
+            printf("%c",a[r][c]);
+
+        }
+        printf("\n");
+    }
+
+
+}
+
